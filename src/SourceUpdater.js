349503bf32a59a0310b09ae28936e026ff9b6480
Paradox <paradoxalcorp@gmail.com>
2019-01-24 16:45:15 +0100

    Use github api to check for updates

--- src/SourceUpdater.js
+++ src/SourceUpdater.js
@@ -3,20 +3,23 @@
 const exec = require("util").promisify(require("child_process").exec);
 const rl = require("readline");
 const { promises: fs } = require("fs");
+const { default: axios } = require("axios");
+const { join } = require('path');
 
 class SourceUpdater {
     constructor() {
         /** @type {import("klasa").KlasaConsole} */
         this.console = new (require("klasa").KlasaConsole)();
         this.config = require("../config");
+        this.cwd = join(__dirname, '..');
     }
 
     async checkForUpdates() {
         if (this.config.updateBehavior === "none") {
             return false;
         }
-        const status = await exec("git status --porcelain=v2").catch(this._handleFailedStatusCheck.bind(this));
-        if (!status.stdout) {
+        const status = await this._updateAvailable().catch(this._handleFailedStatusCheck.bind(this));
+        if (!status) {
             this.console.log(`No updates found, running the latest ${require("../package").version} version of Naka`);
         } else {
             if (this.config.updateBehavior === "prompt") {
@@ -82,6 +85,31 @@ class SourceUpdater {
         delete require.cache[require.resolve("../config")];
         delete require.cache[require.resolve("../config.example")];
         return this.console.log("The config file has been updated with new options, you may want to take a look at it");
+    }
+
+    async _updateAvailable() { // Code shamelessly stolen from https://github.com/aetheryx/powercord/blob/v2/src/Powercord/plugins/pc-updater because git is a pain to deal with
+        const branch = await exec("git branch", this.cwd)
+            .then(({ stdout }) =>
+                stdout
+                    .toString()
+                    .split("\n")
+                    .find(l => l.startsWith("*"))
+                    .slice(2)
+                    .trim()
+            );
+  
+        const localRevision = await exec(`git rev-parse ${branch}`, this.cwd)
+            .then(r => r.stdout.toString().trim());
+  
+        const currentRevision = await axios.get(`https://api.github.com/repos/${this.config.repository}/commits`, {
+            headers: { Accept: "application/vnd.github.v3+json"},
+            params: {
+                sha: branch
+            }
+        })
+            .then(r => r.data[0].sha);
+  
+        return localRevision !== currentRevision;
     }
 }
 

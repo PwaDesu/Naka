c7f02d51ddaa65647237731a720c7bc568c9c40b
Paradox <paradoxalcorp@gmail.com>
2019-01-25 18:34:06 +0100

    working iamnot

--- commands/Misc/iamnot.js
+++ commands/Misc/iamnot.js
@@ -6,16 +6,15 @@ module.exports = class extends Command {
         super(...args, {
             requiredPermissions: ["MANAGE_ROLES", "MANAGE_MESSAGES"],
             description: language => language.get("COMMAND_IAMNOT_DESCRIPTION"),
-            usage: "iam <role_name>",
             runIn: ["text"]
         });
     }
 
-    async run(message, params) {
-        if (!params[0]) {
-            return message.sendLocale("COMMAND_IAMNOT_NO_ROLE_GIVEN");
+    async run(message) {
+        if (!message.args[0]) {
+            return message.sendLocale("COMMAND_IAMNOT_NO_ROLE_GIVEN").then(m => m.delete({ timeout: 5000 }));
         }
-        let role = message.guild.roles.searchAndResolve(params.join(" "), this.client.config.selfAssignableRoles);
+        let role = message.guild.roles.searchAndResolve(message.args.join(" "), this.client.config.selfAssignableRoles);
         if (Array.isArray(role)) {
             let chosen = await message.prompt(message.guild.language.get("COMMANDMESSAGE_MULTIPLE_ROLES_FOUND", role));
             if (!chosen || !role[chosen.content - 1]) {
@@ -25,15 +24,15 @@ module.exports = class extends Command {
             }
             chosen.delete().catch(() => {});
         }
+        if (!role) {
+            return message.sendLocale("COMMANDMESSAGE_NO_ROLE_FOUND").then(m => m.delete({ timeout: 5000 }));
+        }
         if (role.position > message.guild.me.roles.highest.position) {
-            return message.sendLocale("COMMANDMESSAGE_ROLE_HIGHER_THAN_BOT");
+            return message.sendLocale("COMMANDMESSAGE_ROLE_HIGHER_THAN_BOT").then(m => m.delete({ timeout: 5000 }));
         } else if (!message.member.roles.has(role.id)) {
-            return message.sendLocale("COMMAND_IAMNOT_HAS_NOT_ROLE");
+            return message.sendLocale("COMMAND_IAMNOT_HAS_NOT_ROLE").then(m => m.delete({ timeout: 5000 }));
         }
-        await message.member.removeRole(role.id);
-        if (message.channel.id === this.client.config.selectionChannel) {
-            message.delete().catch(() => {});
-        }
-        return message.sendLocale("COMMAND_IAMNOT_ROLE_REMOVED", [role]).then(m => m.delete(5000));
+        await message.member.roles.remove(role.id);
+        return message.sendLocale("COMMAND_IAMNOT_ROLE_REMOVED", [role]).then(m => m.delete({ timeout: 5000 }));
     }
 };
